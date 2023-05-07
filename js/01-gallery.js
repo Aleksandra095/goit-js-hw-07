e3b4ea934d939af871261e4b5310bf1bcb2e2eea
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-07 11:55:07 +0300

    1 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery')
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+}).join('')
 
-console.log(galleryItems);
+galleryList.insertAdjacentHTML('afterbegin', markup);
+
+galleryList.addEventListener("click", onModalOpenImg);
+function onModalOpenImg(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    const largeImg = event.target.dataset.source
+    createModalWindow(largeImg)
+}
+function createModalWindow(url) {
+    const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">`)
+    instance.show()
+    window.addEventListener('keydown', onModalClose);
+    function onModalClose(event) {
+        console.log(event);
+        if (event.code === 'Escape') {
+            instance.close()
+            window.removeEventListener('keydown', onModalClose);
+        }
+    }
+}
