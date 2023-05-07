4d2f4c47463a2f72de7a1aad565ba50433829430
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-07 13:03:05 +0300

    2 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery')
 
-console.log(galleryItems);
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>`
+}).join('')
+
+galleryList.insertAdjacentHTML('afterbegin', markup);
+new SimpleLightbox('.gallery a', { overlayOpacity: 0.8, captionsData: 'alt', captionDelay: 250, showCounter: false });
